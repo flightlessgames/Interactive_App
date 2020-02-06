3e8608d5f3623f9c16be583f1b0b0c2b8114c944
Ben <contact@flightlessgames.net>
2020-02-06 16:20:48 -0600

    v0.1.1 Scriptable Ingredients & Potions
    Added ScriptableObjects Ingredients and Potion assemblies & folders. Pushing to Master before moving onto CraftingTable Feature Branch

---
+++ Demo/Assets/Scripts/ScriptableObjects/Ingredients_sObj.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName ="New_Ingredient_sObj", menuName = "sObj/Ingredient")]
+public class Ingredients_sObj : ScriptableObject
+{
+
+    public Vector3 Values = Vector3.zero;
+    public string Name = "...";
+    public Sprite Image = null;
+}
