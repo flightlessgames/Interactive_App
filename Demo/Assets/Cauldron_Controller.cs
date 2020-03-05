a54f978808a2c0031b10dd51449c4ebad44aed51
Ben <contact@flightlessgames.net>
2020-03-04 19:02:07 -0600

    Test Cauldron
    testing some cauldron scripts

---
+++ Demo/Assets/Cauldron_Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class Cauldron_Controller : MonoBehaviour
+{
+    [Header("Required")]
+    [SerializeField] private _devCrafting _crafting = null;
+
+    [Header("Settings")]
+    [SerializeField] private float _craftDelay = 0.5f;
+
+    public void OnClick()
+    {
+        StartCoroutine(Craft());
+    }
+
+    IEnumerator Craft()
+    {
+        yield return new WaitForSeconds(_craftDelay);
+
+        _crafting.CraftPotion();
+    }
+}
